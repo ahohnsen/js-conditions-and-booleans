ea0a56130c2841416cdb4b690701862c27e552fe
Andrea Hohnsen <a.hohnsen@mail.de>
2024-03-06 12:53:36 +0100

    Add demo-end

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
-let carrots = 2;
+console.clear();
+// if/else - statements
 
-carrots = carrots + 2;
+let carrots = 2;
+const dishesCleaned = 4;
+
+const kidHelpsCleaningTheKitchen = dishesCleaned > 3;
+
+if (kidHelpsCleaningTheKitchen) {
+  carrots = carrots + 2;
+} else {
+  carrots = carrots - 1;
+}
+
+console.log(carrots);
+
+// Ternary Operator
+// condition ? value if true : value if false
+
+/*let carrots = 2;
+const dishesCleaned = 2;
+
+const kidHelpsCleaningTheKitchen = dishesCleaned > 3;
+
+let extraCarrots = kidHelpsCleaningTheKitchen ? 2 : -1;
+
+carrots = carrots + extraCarrots;
+
+console.log(carrots);*/
+
+// Type coercion - truthy and falsy
+
+/*let carrots = 0;
+
+if (carrots) {
+  console.log("Give me the carrots already.");
+}*/
